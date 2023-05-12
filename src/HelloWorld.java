f097d90c384d985248b026031044dd27d74b7cf5
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-05-12 17:29:59 -0400

    exersice complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,22 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        int myFavoriteNumber = 4;
+        String myString = "How dare you!";
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+
+        int x = 10;
+        int y = 2;
+         x /= y;
+         y -= x;
+
+
+
+
+
     }
 }
