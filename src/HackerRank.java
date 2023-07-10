7bc0c12c6629adc17c48db4ea8f86ee89301570d
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-07-09 23:13:01 -0400

    working on some hacker rank practice

---
+++ src/HackerRank.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HackerRank {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//        List<String> lines = new ArrayList<>();
+//        int numToAdd = 0;
+//        while (sc.hasNextLine()) {
+//            numToAdd += 1;
+//           String line = numToAdd + sc.nextLine();
+//           lines.add(line);
+//        }
+//        for (String line : lines) {
+//            System.out.println(line);
+//        }
+//    }
+//        int B = sc.nextInt();
+//        int H = sc.nextInt();
+//
+//        if (B <= 0 || H <= 0) {
+//            System.out.println("java.lang.Exception: Breadth and height must be positive");
+//        } else {
+//            int area = B * H;
+//            System.out.println(area);
+//        }
+        int n = sc.nextInt();
+        if (n >= -100 && n <= 100){
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer");
+
+    }
+}
+
+
